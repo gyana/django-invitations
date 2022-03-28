2c231ad9a6db37b13a94268543a87deffa79531f
Fernado Cordeiro <fernando_cordeiro@live.com>
2022-03-28 15:51:14 +0100

    👕 LINT: Black

--- invitations/urls.py
+++ invitations/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'invitations'
+app_name = "invitations"
 urlpatterns = [
-    path('send-invite/', views.SendInvite.as_view(), name='send-invite'),
-    path('send-json-invite/', views.SendJSONInvite.as_view(), name='send-json-invite'),
-    path('accept-invite/<key>/?', views.AcceptInvite.as_view(), name='accept-invite'),
+    path("send-invite/", views.SendInvite.as_view(), name="send-invite"),
+    path("send-json-invite/", views.SendJSONInvite.as_view(), name="send-json-invite"),
+    path("accept-invite/<key>/?", views.AcceptInvite.as_view(), name="accept-invite"),
 ]
--- test_urls.py
+++ test_urls.py
@@ -5,11 +5,9 @@ from django.urls import include, path
 admin.autodiscover()
 
 urlpatterns = [
-    path('invitations/', include('invitations.urls')),
-    path('admin/', admin.site.urls),
+    path("invitations/", include("invitations.urls")),
+    path("admin/", admin.site.urls),
 ]
 
-if 'allauth' in settings.INSTALLED_APPS:
-    urlpatterns.append(
-        path('accounts/', include('allauth.urls'))
-    )
+if "allauth" in settings.INSTALLED_APPS:
+    urlpatterns.append(path("accounts/", include("allauth.urls")))
